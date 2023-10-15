9dc4c5fcc7357a233f509bd3f7ca6e06a42696df
Berkay Tuncel <berkaytncl98@gmail.com>
2023-10-15 16:26:06 +0300

    Create views with dummy datas

---
+++ TodoList/Views/AddView.swift
@@ -0,0 +1,46 @@
+//
+//  AddView.swift
+//  TodoList
+//
+//  Created by Berkay Tuncel on 15.10.2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color.gray.opacity(0.2))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Button {
+                    
+                } label: {
+                    Text("Save".uppercased())
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+
+            }
+            .padding(16)
+        }
+        .navigationTitle("Add an Item 🖋️")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AddView()
+    }
+}
