8268e1038a91e05366b048e318aa4124d25657c0
Berkay Tuncel <berkaytncl98@gmail.com>
2023-10-16 14:17:09 +0300

    Add ViewModel and crud functions

---
+++ TodoList/ViewModels/ListViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  ListViewModel.swift
+//  TodoList
+//
+//  Created by Berkay Tuncel on 16.10.2023.
+//
+
+import Foundation
+
+/*
+ CRUD FUNCTIONS
+ 
+ Create
+ Read
+ Update
+ Delete
+ */
+
+class ListViewModel: ObservableObject {
+    
+    @Published var items: [ItemModel] = []
+    
+    init() {
+        getItems()
+    }
+    
+    private func getItems() {
+        let newItems = [
+            ItemModel(title: "This is the first title!", isCompleted: false),
+            ItemModel(title: "This is the second title!", isCompleted: true),
+            ItemModel(title: "This is the third title!", isCompleted: false)
+        ]
+        
+        items.append(contentsOf: newItems)
+    }
+    
+    func addItem(title: String) {
+        let newItem = ItemModel(title: title, isCompleted: false)
+        items.append(newItem)
+    }
+    
+    func updateItem(item: ItemModel) {
+        guard let index = items.firstIndex(where: { $0.id == item.id }) else { return }
+        items[index] = item.updateCompletion()
+    }
+    
+    func moveItem(from: IndexSet, to: Int) {
+        items.move(fromOffsets: from, toOffset: to)
+    }
+    
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+}
--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.dismiss) var dismissScreen
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -20,9 +25,7 @@ struct AddView: View {
                     .background(Color.gray.opacity(0.2))
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                 
-                Button {
-                    
-                } label: {
+                Button(action: saveButtonPressed) {
                     Text("Save".uppercased())
                         .foregroundStyle(.white)
                         .font(.headline)
@@ -31,16 +34,30 @@ struct AddView: View {
                         .background(Color.accentColor)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
-
             }
             .padding(16)
         }
         .navigationTitle("Add an Item 🖋️")
+        .alert(alertTitle, isPresented: $showAlert) {}
     }
+    
+    private func saveButtonPressed() {
+        guard textIsAppropriate() else {
+            alertTitle = "Your new todo item must be at least 3 characters long!! 😨😰😱"
+            showAlert.toggle()
+            return
+        }
+        
+        listViewModel.addItem(title: textFieldText)
+        dismissScreen.callAsFunction()
+    }
+    
+    private func textIsAppropriate() -> Bool { textFieldText.count >= 3 }
 }
 
 #Preview {
     NavigationStack {
         AddView()
     }
+    .environmentObject(ListViewModel())
 }
